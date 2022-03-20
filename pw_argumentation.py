50aafb6d4172a015df5f0e01c75aa8db33fc5539
Jérôme AUGUSTE <jerome_auguste@outlook.fr>
2022-03-20 19:03:03 +0100

    6.2 4 Simple PROPOSE ACCEPT behaviour, WIP best practices for implementation

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -1,4 +1,3 @@
-from os import name
 from mesa import Model
 from mesa.time import RandomActivation
 
@@ -9,63 +8,94 @@ from communication.preferences.Item import Item
 from communication.preferences.CriterionValue import CriterionValue
 from communication.preferences.CriterionName import CriterionName
 from communication.preferences.Value import Value
+from communication.message.Message import Message
+from communication.message.MessagePerformative import MessagePerformative
 
 class ArgumentAgent(CommunicatingAgent):
     """ArgumentAgent which inherits from CommunicatingAgent"""
-    
+
     def __init__(self, unique_id, model, name):
         super().__init__(unique_id=unique_id, model=model, name=name)
         self.preference = Preferences()
-    
+
     def step(self):
         super().step()
-    
+        # print(f"{self.get_name()}'s turn")
+        list_messages = self.get_new_messages()
+        for message in list_messages:
+            print(message)
+            if message.get_performative() == MessagePerformative.PROPOSE and message.get_content() in self.model.items_list:
+                self.send_message(Message(from_agent=self.get_name(), to_agent=message.get_exp(), message_performative=MessagePerformative.ACCEPT, content=message.get_content()))
+            # if message.get_performative() == MessagePerformative.ACCEPT and message.get_content() in self.model.items_list:
+                
+
     def get_preference(self):
         return self.preference
-    
+
     def generate_preferences(self):
         # see question 3
         # To be completed
-        
+        # TODO: Add a csv filename argument to generate preferences (need to discuss about the formatting)
+
         # Random criterion priorities
         criterion_list = list(CriterionName)
         self.random.shuffle(criterion_list)
         self.preference.set_criterion_name_list(criterion_list)
-        
+
         # Random generation of preferences for all criteria
         for item in self.model.items_list:
             for criterion in self.preference.get_criterion_name_list():
                 item_criterion_pref = CriterionValue(item, criterion, self.random.choice(list(Value)))
                 self.preference.add_criterion_value(item_criterion_pref)
-    
+
 class ArgumentModel(Model):
     """ArgumentModel which inherits from Model"""
-    
+
     def __init__(self, items_list, list_agent_names):
         super().__init__(self)
         self.schedule = RandomActivation(self)
         self.__message_service = MessageService(self.schedule)
         self.items_list = items_list
         self.__list_agent_names = list_agent_names
-        
-        # To be completed
-        
-        for agent_name in self.__list_agent_names:
+        self._name_to_id = {}
 
-            a = ArgumentAgent(self.next_id(), self, agent_name)
+        # To be completed
+
+        for agent_name in self.__list_agent_names:
+            new_id = self.next_id()
+            a = ArgumentAgent(new_id, self, agent_name)
             a.generate_preferences()
             self.schedule.add(a)
+            self._name_to_id[agent_name] = new_id
 
         self.running = True
-        
+
+    # def argue(self):
+    #     a_1_agent = self.schedule._agents[self._name_to_id["A_1"]]
+    #     a_1_agent.send_message(
+    #         Message(from_agent=a_1_agent.get_name(),
+    #                 to_agent="A_2",
+    #                 message_performative=MessagePerformative.PROPOSE,
+    #                 content=self.random.choice(self.items_list)))
+
     def step(self):
+        # self.argue()
         self.__message_service.dispatch_messages()
         self.schedule.step()
-        
+
 if __name__ == "__main__":
     # To be completed
     items_list = [Item("ICED", "Diesel"), Item("E", "Electric")]
     agents_name = ["A_1", "A_2"]
     model = ArgumentModel(items_list=items_list, list_agent_names=agents_name)
-    model.step()
     
+    a_1_agent = model.schedule._agents[model._name_to_id["A_1"]]
+    a_1_agent.send_message(
+        Message(from_agent=a_1_agent.get_name(),
+                to_agent="A_2",
+                message_performative=MessagePerformative.PROPOSE,
+                content=model.random.choice(model.items_list)))
+    
+    # Management of RandomActivation ?
+    for _ in range(10):
+        model.step()
