0e9626705aa215917a365d01e1f5c3fc6c20fb08
Magali Morin <magali.morin@student-cs.fr>
2022-04-15 17:55:49 +0200

    Work in progress to Use only once the arguments

--- arguments/Argument.py
+++ arguments/Argument.py
@@ -41,60 +41,56 @@ class Argument:
         """Add a premiss couple values in the couple values list.
         """
         self.__couple_values_list.append(CoupleValue(criterion_name, value))
-    
+
     def get_premiss_couple_values_list(self):
         """Returns the premiss couple values list.
         """
         return self.__couple_values_list
-    
+
     def get_premiss_comparison_list(self):
         """Returns the premiss comparison list.
         """
         return self.__comparison_list
-    
+
     def get_premisses(self):
         return self.__premisses
-    
+
     def get_conclusion(self):
         return self.__decision, self.__item
 
-    def list_supporting_proposal(self, preference):
+    def list_supporting_proposal(self, preference, usedArguments):
         """ Generate a list of premisses which can be used to support an item
             : param item : Item - name of the item
             : return : list of all premisses PRO an item ( sorted by order of importance
                 based on agent's preferences )
         """
         criterion_list = preference.get_criterion_name_list()
-        # supporting_proposal = []
         for criterion_name in criterion_list:
             pref_value = preference.get_value(self.__item, criterion_name)
-            if pref_value in [Value.GOOD, Value.VERY_GOOD]:
-        #         supporting_proposal.append(CoupleValue(criterion_name, pref_value))
-        # return supporting_proposal
+            # print('(CoupleValue(criterion_name, pref_value) in usedArguments ?', str(CoupleValue(criterion_name, pref_value)), usedArguments,
+            #   str(CoupleValue(criterion_name, pref_value)) not in usedArguments[self.__item.get_name()])
+            if (pref_value in [Value.GOOD, Value.VERY_GOOD]) and (str(CoupleValue(criterion_name, pref_value)) not in usedArguments[self.__item.get_name()]):
                 self.add_premiss_couple_values(
                     criterion_name, preference.get_value(self.__item, criterion_name))
-                # for worse_criterion_name in criterion_list:
-                #     if preference.is_preferred_criterion(criterion_name, worse_criterion_name):
-                #         self.add_premiss_comparison(
-                #             criterion_name, worse_criterion_name)
 
-    def list_attacking_proposal(self, preference):
+    def list_attacking_proposal(self, preference, usedArguments):
         """ Generate a list of premisses which can be used to attack an item
             : param item : Item - name of the item
             : return : list of all premisses CON an item ( sorted by order of importance
             based on preferences )
         """
         criterion_list = preference.get_criterion_name_list()
-        # attacking_proposal = []
         for criterion_name in criterion_list:
             pref_value = preference.get_value(self.__item, criterion_name)
-            if pref_value in [Value.BAD, Value.VERY_BAD]:
-        #         attacking_proposal.append(CoupleValue(criterion_name, pref_value))
-        # return attacking_proposal
+            print('pref_value', pref_value, pref_value in [
+                  Value.BAD, Value.VERY_BAD])
+            print('couple_value', CoupleValue(criterion_name, pref_value), str(CoupleValue(
+                criterion_name, pref_value)) not in usedArguments[self.__item.get_name()])
+            if (pref_value in [Value.BAD, Value.VERY_BAD]) and (str(CoupleValue(criterion_name, pref_value)) not in usedArguments[self.__item.get_name()]):
                 self.add_premiss_couple_values(
                     criterion_name, preference.get_value(self.__item, criterion_name))
                 for better_criterion in criterion_list:
-                    if preference.is_preferred_criterion(better_criterion, criterion_name):
+                    if (preference.is_preferred_criterion(better_criterion, criterion_name)) and (str(Comparison(better_criterion, criterion_name)) not in usedArguments[self.__item.get_name()]):
                         self.add_premiss_comparison(
                             better_criterion, criterion_name)
 
@@ -104,10 +100,9 @@ class Argument:
         elif isinstance(premiss, Comparison) and premiss in self.__comparison_list:
             self.__premisses.append(premiss)
         else:
-            self.__premisses.append(self.__couple_values_list[0])
-            
+            self.__premisses.append(self.__couple_values_list[-1])
+
         # if is_couple_value_type and len(self.__couple_values_list) > 0:
         #     self.__premisses.append(self.__couple_values_list[premiss_idx])
         # elif not is_couple_value_type and len(self.__comparison_list) > 0:
         #     self.__premisses.append(self.__comparison_list[premiss_idx])
-        
--- pw_argumentation.py
+++ pw_argumentation.py
@@ -1,5 +1,6 @@
 from mesa import Model
 from mesa.time import RandomActivation
+import re
 
 from communication.agent.CommunicatingAgent import CommunicatingAgent
 from communication.message.MessageService import MessageService
@@ -18,10 +19,12 @@ from arguments.CoupleValue import CoupleValue
 class ArgumentAgent(CommunicatingAgent):
     """ArgumentAgent which inherits from CommunicatingAgent"""
 
-    def __init__(self, unique_id, model, name):
+    def __init__(self, unique_id, model, name, _item_set):
         super().__init__(unique_id=unique_id, model=model, name=name)
         self.preference = Preferences()
         self.__committed = False
+        self.__usedArguments = [{key.get_name(): [] for key in _item_set}, {
+            key.get_name(): [] for key in _item_set}]
 
     def step(self):
         # TODO: Add unit tests
@@ -30,9 +33,6 @@ class ArgumentAgent(CommunicatingAgent):
         list_messages = self.get_new_messages()
         for message in list_messages:
             print(message)
-            # print(f"Agent {self.get_name()} has right performative: {message.get_performative() in [MessagePerformative.ACCEPT, MessagePerformative.COMMIT]}")
-            # print(f"Agent {self.get_name()} has item in set: {message.get_content() in self._item_set}")
-            # print(f"Agent {self.get_name()} has item set: {self._item_set}")
             if (message.get_performative() == MessagePerformative.PROPOSE):
                 if self.preference.is_item_among_top_10_percent(message.get_content(), self.model._item_set):
                     self.send_message(Message(from_agent=self.get_name(), to_agent=message.get_exp(
@@ -42,7 +42,6 @@ class ArgumentAgent(CommunicatingAgent):
                     ), message_performative=MessagePerformative.ASK_WHY, content=message.get_content()))
 
             if (message.get_performative() == MessagePerformative.ACCEPT):
-                # print(f"Got through that condition for {self.get_name()}")
                 self.send_message(Message(from_agent=self.get_name(), to_agent=message.get_exp(
                 ), message_performative=MessagePerformative.COMMIT, content=message.get_content()))
                 self.__committed = True
@@ -58,12 +57,21 @@ class ArgumentAgent(CommunicatingAgent):
 
             if message.get_performative() == MessagePerformative.ARGUE:
                 argument = message.get_content()
-                # TODO: Send message
-                attack_argument = self.attack_proposal(argument)
-                if attack_argument:
-                    self.send_message(Message(from_agent=self.get_name(), to_agent=message.get_exp(), message_performative=MessagePerformative.ARGUE, content=attack_argument))
+                item_name = re.search('(.+?)←', str(argument)).group(1)
+                item_class = self.model.find_item_from_name(item_name)
+                (decision, _) = argument.get_conclusion()  # item
+                if not decision:
+                    response = self.support_proposal(item_class)
                 else:
-                    self.send_message(Message(from_agent=self.get_name(), to_agent=message.get_exp(), message_performative=MessagePerformative.ACCEPT, content=argument.get_conclusion()[1]))
+                    response = self.attack_proposal(argument)
+
+                # DONE: Faire en sorte que ça renvoit un attacking ou un supporting en fonction de la décision
+                if response:
+                    self.send_message(Message(from_agent=self.get_name(), to_agent=message.get_exp(
+                    ), message_performative=MessagePerformative.ARGUE, content=response))
+                else:
+                    self.send_message(Message(from_agent=self.get_name(), to_agent=message.get_exp(
+                    ), message_performative=MessagePerformative.ACCEPT, content=argument.get_conclusion()[1]))
 
     def get_preference(self):
         return self.preference
@@ -85,54 +93,65 @@ class ArgumentAgent(CommunicatingAgent):
             # Random generation of preferences for all criteria
             for item in self.model._item_set:
                 for criterion in self.preference.get_criterion_name_list():
-                    item_criterion_pref = CriterionValue(item, criterion, self.random.choice(list(Value)))
+                    item_criterion_pref = CriterionValue(
+                        item, criterion, self.random.choice(list(Value)))
                     self.preference.add_criterion_value(item_criterion_pref)
         else:
             if self.get_name() == "A_1":
-                self.preference.set_criterion_name_list([CriterionName.PRODUCTION_COST, CriterionName.ENVIRONMENT_IMPACT, CriterionName.CONSUMPTION, CriterionName.DURABILITY, CriterionName.NOISE])
+                self.preference.set_criterion_name_list(
+                    [CriterionName.PRODUCTION_COST, CriterionName.ENVIRONMENT_IMPACT, CriterionName.CONSUMPTION, CriterionName.DURABILITY, CriterionName.NOISE])
                 for item in self.model._item_set:
                     if item.get_name() == "ICED":
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.PRODUCTION_COST, Value.VERY_GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.CONSUMPTION, Value.GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.DURABILITY, Value.VERY_GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.ENVIRONMENT_IMPACT, Value.VERY_BAD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.NOISE, Value.BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.PRODUCTION_COST, Value.GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.CONSUMPTION, Value.BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.DURABILITY, Value.BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.ENVIRONMENT_IMPACT, Value.BAD))
+                        self.preference.add_criterion_value(
+                            CriterionValue(item, CriterionName.NOISE, Value.BAD))
                     elif item.get_name() == "E":
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.PRODUCTION_COST, Value.BAD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.CONSUMPTION, Value.VERY_BAD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.DURABILITY, Value.GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.ENVIRONMENT_IMPACT, Value.VERY_GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.NOISE, Value.VERY_GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.PRODUCTION_COST, Value.GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.CONSUMPTION, Value.VERY_BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.DURABILITY, Value.BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.ENVIRONMENT_IMPACT, Value.BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.NOISE, Value.BAD))
             elif self.get_name() == "A_2":
+                self.preference.set_criterion_name_list(
+                    [CriterionName.PRODUCTION_COST, CriterionName.ENVIRONMENT_IMPACT, CriterionName.CONSUMPTION, CriterionName.DURABILITY, CriterionName.NOISE])
                 for item in self.model._item_set:
                     if item.get_name() == "ICED":
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.PRODUCTION_COST, Value.GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.CONSUMPTION, Value.BAD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.DURABILITY, Value.VERY_GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.ENVIRONMENT_IMPACT, Value.VERY_BAD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.NOISE, Value.VERY_BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.PRODUCTION_COST, Value.VERY_BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.CONSUMPTION, Value.GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.DURABILITY, Value.GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.ENVIRONMENT_IMPACT, Value.GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.NOISE, Value.GOOD))
                     elif item.get_name() == "E":
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.PRODUCTION_COST, Value.GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.CONSUMPTION, Value.BAD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.DURABILITY, Value.BAD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.ENVIRONMENT_IMPACT, Value.VERY_GOOD))
-                        self.preference.add_criterion_value(CriterionValue(item, CriterionName.NOISE, Value.VERY_GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.PRODUCTION_COST, Value.VERY_BAD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.CONSUMPTION, Value.GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.DURABILITY, Value.GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.ENVIRONMENT_IMPACT, Value.GOOD))
+                        self.preference.add_criterion_value(CriterionValue(
+                            item, CriterionName.NOISE, Value.GOOD))
             else:
                 self.generate_preferences(self, random_prefs=True)
 
-    def support_proposal(self, item):
-        """
-        Used when the agent receives " ASK_WHY " after having proposed an item
-        : param item_name : str - name of the item which was proposed
-        : return : string - the strongest supportive argument
-        """
-        # item = self.model.find_item_from_name(item_name)
-        argument = Argument(True, item)
-        argument.list_supporting_proposal(self.preference)
-        
-        argument.add_premiss()
-        return argument
-    
     def argument_parsing(self, argument):
         """returns the premisses and the decision from recieved message
 
@@ -143,48 +162,99 @@ class ArgumentAgent(CommunicatingAgent):
             premisses: list of premisses
             conclusion: the decision (boolean, item)
         """
-        return argument.get_premisses(), argument.get_conclusion()         
+        return argument.get_premisses(), argument.get_conclusion()
 
-    def attack_proposal(self, argument):
-        premisses, (decision, proposed_item) = self.argument_parsing(argument)
+    def support_proposal(self, item):  # type item : Item
+        """
+        Used when the agent receives " ASK_WHY " after having proposed an item
+        : param item : Item - item of the item which was proposed
+        : return : string - the strongest supportive argument
+        """
+        if self.get_name() == 'A_1':
+            usedArguments = self.__usedArguments[0]
+        else:
+            usedArguments = self.__usedArguments[1]
+        print(self.get_name())
+        print('used_argument in SUPPORT PROPOSAL', usedArguments)
+        argument = Argument(True, item)
+        argument.list_supporting_proposal(
+            self.preference, usedArguments)
+        argument.add_premiss()
+        usedArguments[item.get_name()].append(
+            str(argument).split('←  ')[1])
+        # argument.get_premiss Regarder si y'a besoin de mettre le booleen, dico en clé item et argument les premiss utilisées
+        print(usedArguments)
+        return argument
+
+    def attack_proposal(self, argument):  # type item : Item
+        if self.get_name() == 'A_1':
+            usedArguments = self.__usedArguments[0]
+        else:
+            usedArguments = self.__usedArguments[1]
+        print(self.get_name())
+        print('used_argument in ATTACK PROPOSAL', usedArguments)
+        premisses, (_, proposed_item) = self.argument_parsing(argument)
         criterion_preference_order = self.preference.get_criterion_name_list()
+        print('criterion_pref_order', criterion_preference_order)
         attacking_argument = Argument(False, proposed_item)
-        attacking_argument.list_attacking_proposal(self.preference)
+        print('attacking_arg', attacking_argument)
+        attacking_argument.list_attacking_proposal(
+            self.preference, usedArguments)
+        print('attack_arg_premisses', attacking_argument.get_premisses())
+
         for premiss in premisses:
             if isinstance(premiss, CoupleValue):
-
                 # Prefered criterion
-                for criterion in criterion_preference_order:
+                for criterion in reversed(criterion_preference_order):
                     if self.preference.is_preferred_criterion(criterion, premiss.get_criterion_name()):
-                        comp = Comparison(criterion, premiss.get_criterion_name())
+                        comp = Comparison(
+                            criterion, premiss.get_criterion_name())
                         attacking_argument.add_premiss(comp)
+                        usedArguments[item.get_name()].append(
+                            str(attacking_argument).split('←  ')[1])
+                        print('used_argument in ATTACK PROPOSAL', usedArguments)
                         return attacking_argument
-                
+
                 # Lower value for the same criterion
+                premiss = CoupleValue(premiss.get_criterion_name(), self.preference.get_value(
+                    proposed_item, premiss.get_criterion_name()))
                 if self.preference.get_value(proposed_item, premiss.get_criterion_name()).value < premiss.get_value().value:
-                    premiss = CoupleValue(premiss.get_criterion_name(), self.preference.get_value(proposed_item, premiss.get_criterion_name()))
                     attacking_argument.add_premiss(premiss)
+                    usedArguments[item.get_name()].append(
+                        str(attacking_argument).split('←  ')[1])
+                    print('used_argument in ATTACK PROPOSAL', usedArguments)
                     return attacking_argument
-                
+
                 # Prefered criterion has low value
-                for criterion in criterion_preference_order:
+                for criterion in reversed(criterion_preference_order):
                     if self.preference.is_preferred_criterion(criterion, premiss.get_criterion_name()) and self.preference.get_value(proposed_item, premiss.get_criterion_name()) in [Value.BAD, Value.VERY_BAD]:
-                        better_crit = Comparison(criterion, premiss.get_criterion_name())
-                        bad_value = CoupleValue(criterion, self.preference.get_value(proposed_item, premiss.get_criterion_name()))
+                        better_crit = Comparison(
+                            criterion, premiss.get_criterion_name())
+                        bad_value = CoupleValue(criterion, self.preference.get_value(
+                            proposed_item, premiss.get_criterion_name()))
                         attacking_argument.add_premiss(better_crit)
                         attacking_argument.add_premiss(bad_value)
+                        usedArguments[item.get_name()].append(
+                            str(attacking_argument).split('←  ')[1])
+                        print('used_argument in ATTACK PROPOSAL', usedArguments)
                         return attacking_argument
-                
+
                 # Other item with better value on the same criterion
                 for item in self.model._item_set:
                     if self.preference.get_value(item, premiss.get_criterion_name()).value > premiss.get_value().value:
                         other_prop = Argument(True, item)
                         other_prop.list_supporting_proposal(self.preference)
-                        better_value = CoupleValue(premiss.get_criterion_name(), self.preference.get_value(item, premiss.get_criterion_name()))
+                        better_value = CoupleValue(premiss.get_criterion_name(
+                        ), self.preference.get_value(item, premiss.get_criterion_name()))
                         other_prop.add_premiss(better_value)
+                        usedArguments[item.get_name()].append(
+                            str(other_prop).split('←  ')[1])
+                        print('used_argument in ATTACKING PROPOSAL',
+                              usedArguments)
                         return other_prop
 
         return False
+
 
 class ArgumentModel(Model):
     """ArgumentModel which inherits from Model"""
@@ -196,17 +266,16 @@ class ArgumentModel(Model):
         self._item_set = item_set.copy()
         self.__list_agent_names = list_agent_names.copy()
         self._name_to_id = {}
-        
-        
+        self.iteration = 0
+
         # To be completed
 
         for agent_name in self.__list_agent_names:
             new_id = self.next_id()
-            a = ArgumentAgent(new_id, self, agent_name)
-            a.generate_preferences()
+            a = ArgumentAgent(new_id, self, agent_name, self._item_set)
+            a.generate_preferences(random_prefs=False)
             self.schedule.add(a)
             self._name_to_id[agent_name] = new_id
-
         self.running = True
 
     # Attempt at handling negociation using model step
@@ -224,18 +293,20 @@ class ArgumentModel(Model):
             if not agent.has_committed():
                 return
         self.running = False
-    
-    def find_item_from_name(self, item_name):
+
+    def find_item_from_name(self, item_name):  # type de item_name : str
         for item in self._item_set:
-            if item.get_name() == item_name:
+            if item.get_name() == item_name.replace(' ', ''):
                 return item
 
     def step(self):
         # self.argue()
+        self.iteration += 1
+        if self.iteration > 10:
+            self.running = False
         self.__message_service.dispatch_messages()
         self.check_close_argumentation()
         self.schedule.step()
-        
 
 
 if __name__ == "__main__":
